bd0b1c43e8ea6488d970f71d2ec31595261de5a8
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2023-09-13 16:08:52 +0530

    16.0.0

--- app/src/main/java/com/example/jitpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jitpacktest/MainActivity.kt
@@ -3,17 +3,16 @@ package com.example.jitpacktest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-//import com.example.mylibrary.LibraryClass
 
 class MainActivity : AppCompatActivity() {
-    var a = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val dId = LibraryClass().getDeviceId(applicationContext)
+//        val dId =
 //        Log.e("DID", dId)
+
 
 
     }
