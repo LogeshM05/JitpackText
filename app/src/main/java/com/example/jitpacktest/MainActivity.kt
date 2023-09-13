3738e89433d9edecf02650665f5648a4d232c032
LogeshM05 <logeshwarlogs05@gmail.com>
2023-09-13 12:16:56 +0530

    first commit

---
+++ app/src/main/java/com/example/jitpacktest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.jitpacktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    var a = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        a = add1(5,10) as Int;
+    }
+
+    private fun add1(i: Int, i1: Int): Any {
+
+        Log.e("added value", a.toString())
+
+        return TODO("Provide the return value")
+    }
+}
