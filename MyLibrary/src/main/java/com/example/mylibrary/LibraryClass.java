7805f6546f2f45af63d021da1be815d63d034e63
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2023-09-13 16:49:03 +0530

    19.0.0

--- MyLibrary/src/main/java/com/example/mylibrary/LibraryClass.java
+++ MyLibrary/src/main/java/com/example/mylibrary/LibraryClass.java
@@ -5,9 +5,9 @@ import android.content.Context;
 import android.provider.Settings;
 
 public class LibraryClass {
-    @SuppressLint("HardwareIds")
+
     public String getDeviceId(Context context){
-        String deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        @SuppressLint("HardwareIds") String deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
         return deviceId;
     }
     public void sample(){
