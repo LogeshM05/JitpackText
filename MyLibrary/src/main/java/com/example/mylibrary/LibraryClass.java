20e8409b90d8c73a294b565fffb32c50b8423b92
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2023-09-14 12:47:47 +0530

    28.0.0

--- MyLibrary/src/main/java/com/example/mylibrary/LibraryClass.java
+++ MyLibrary/src/main/java/com/example/mylibrary/LibraryClass.java
@@ -2,7 +2,10 @@ package com.example.mylibrary;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 public class LibraryClass {
 
@@ -21,15 +24,25 @@ public class LibraryClass {
 
             if (user != null) {
                 user = user.replace("api_key_", "");
-                SharedPref.getInstance().setSharedValue(context, "sharedAPIKey", user);
+                Log.e("user", user);
             } else {
                 user = "";
-                Toast.makeText(context, "Please add your SDK API KEY", 1).show();
+                Toast.makeText(context, "Please add your SDK API KEY", Toast.LENGTH_SHORT).show();
             }
 
             return user;
         } catch (Exception var2) {
             return "";
+        }
+    }
+
+    private static String getMetadata(Context context, String name) {
+        try {
+            ApplicationInfo applicationInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(), 128);
+            return applicationInfo.metaData != null && applicationInfo.metaData.containsKey(name) ? applicationInfo.metaData.getString(name, (String)null) : null;
+        } catch (Exception var3) {
+            ExceptionTracker.track(var3);
+            return null;
         }
     }
 }
